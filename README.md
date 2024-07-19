3d9b1ecd4dd45ed754bd98d77eabda212041e3c9
AA <aalborgil002@gmail.com>
2024-07-19 14:39:19 -0400

    Set up handlebars

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # MVC-practice
 A simple project to practice setting up a project using the MVC structure.
 
-<!-- Continue in video 01:37:00 -->
+<!-- Continue in video 02:04:00 Front-end "views"-->
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
 const api = require('./api');
+const Task = require('../models/Task');
 
 // localhost:3001/api
 router.use("/api", api);
+
+router.get('/', async (req, res) => {
+    const allTasks = await Task.findAll();
+    const sanitizedData = allTasks.map((task) => {
+        return task.get({ plain: true });
+    })
+    console.log(allTasks);
+    console.log('========================');
+    console.log(sanitizedData);
+    // const testData = {
+    //     name: "Test Work",
+    //     description: "Work to do",
+    //     status: "In progress"
+    // };
+    res.render('To do', { testData });
+})
 
 // localhost:3001/pikachu
 router.get("/pikachu", (req, res) => {
